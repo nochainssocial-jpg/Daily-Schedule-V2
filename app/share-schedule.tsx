9eb4ef0ed984c60dc9bca324a75ab2e0524751ca
Bruno Pouzet <nochains.social@gmail.com>
2025-11-17 10:45:44 +1100

    Update share-schedule.tsx

--- app/share-schedule.tsx
+++ app/share-schedule.tsx
@@ -201,8 +201,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#faf7fb',
   },
   scroll: {
-    paddingVertical: 24,
-    alignItems: 'center',
+  paddingVertical: 24,
+  alignItems: 'center',
+  paddingBottom: 160,   // 👈 added
   },
   inner: {
     width: '100%',
