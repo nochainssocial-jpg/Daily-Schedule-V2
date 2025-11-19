ab83d5973bf7571280dc61a9064d4907d690b74a
Bruno Pouzet <nochains.social@gmail.com>
2025-11-20 09:25:44 +1100

    Update share-schedule.tsx

--- app/share-schedule.tsx
+++ app/share-schedule.tsx
@@ -222,7 +222,7 @@ export default function ShareScheduleScreen() {
       {/* Large washed-out background logo – web only */}
       {showWebBranding && (
         <Image
-          source={require('../assets/images/No Chains Pink Horizontal final.svg')}
+          source={require('../assets/images/nochains-bg.png')}
           style={styles.bgLogo}
           resizeMode="contain"
         />
