a5c7e16b72d5c25da54f3d8a1e29d6e619bd1e80
Bruno Pouzet <nochains.social@gmail.com>
2025-11-20 09:31:39 +1100

    Update index.tsx

--- app/edit/index.tsx
+++ app/edit/index.tsx
@@ -168,7 +168,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: 1400,
     height: 1400,
-    opacity: 0.06,
+    opacity: 0.1,
     left: -260,
     top: 220,
     pointerEvents: 'none',
