374029c72b51aeb1f5cb575cc73f7df03de99911
Bruno Pouzet <nochains.social@gmail.com>
2025-12-12 06:50:03 +1100

    Update help.tsx

--- app/help.tsx
+++ app/help.tsx
@@ -245,7 +245,7 @@ export default function HelpScreen() {
               {/* Floating */}
               <View style={styles.editCard}>
                 <View style={[styles.iconBubble, { backgroundColor: '#FDF2FF' }]}>
-                  <Ionicons name="account-convert-outline" size={20} color="#4B5563" />
+                  <Ionicons name="account-clock" size={20} color="#4B5563" />
                 </View>
                 <View style={{ flex: 1 }}>
                   <Text style={styles.editCardTitle}>
